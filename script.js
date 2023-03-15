2adb50703b41bc6390401b94e206b3bfabb30237
gabrielemarchetti00 <gabriele.marchetti26@gmail.com>
2023-03-15 10:59:28 +0100

    fix passw check

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-let btn = document.querySelector('button');
 let errorMsg = document.querySelector('.error');
+let pwd = document.querySelector('#password');
+let rpwd = document.querySelector('#conf-pwd');
+if(pwd.textContent == '' && rpwd.textContent == '') {
+    errorMsg.textContent = 'Password do not match';
+}
 
+let btn = document.querySelector('button');
 btn.addEventListener('click', () => {
-    let pwd = document.querySelector('#password');
-    let rpwd = document.querySelector('#conf-pwd');
+    pwd = document.querySelector('#password');
+    rpwd = document.querySelector('#conf-pwd');
     if(pwd != rpwd) {
         errorMsg.textContent = 'Password do not match';
     }
